b687e8b6fff49cee3df92d2e11b80d9dc3333be2
unknown <jgarridb@SVQ-49L95S1.usersad.everis.int>
2014-06-06 13:36:45 +0200

    reseteo

--- src/main/resources/archetype-resources/src/main/java/authenticator/ConstantesAutenticator.java
+++
@@ -1,8 +0,0 @@
-#set( $symbol_pound = '#' )
-#set( $symbol_dollar = '$' )
-#set( $symbol_escape = '\' )
-package ${package}.authenticator;
-
-public class ConstantesAutenticator {
-	public static final String PASS="beebusinesssl";
-}
